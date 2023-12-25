be568cb8d87a58a633956be1d51f5ec79a5fe37e
dkool <dkool@dkools-MacBook-Pro.local>
2023-12-25 11:07:42 +0100

    add another one

--- coordinates-dive/main.go
+++ coordinates-dive/main.go
@@ -9,4 +9,54 @@
 
 package main
 
-func main() {}
+import (
+	"fmt"
+	"strings"
+)
+
+type Submarine struct {
+	HorizontalPosition int
+	Depth              int
+}
+
+func (s *Submarine) Forward(x int) {
+	s.HorizontalPosition += x
+}
+
+func (s *Submarine) Down(x int) {
+	s.Depth += x
+}
+
+func (s *Submarine) Up(x int) {
+	s.Depth -= x
+}
+
+func main() {
+	sub := Submarine{0, 0}
+	commands := []string{
+		"forward 5",
+		"down 5",
+		"forward 8",
+		"up 3",
+		"down 8",
+		"forward 2",
+	}
+
+	for _, cmd := range commands {
+		parts := strings.Fields(cmd)
+		var value int
+		fmt.Sscanf(parts[1], "%d", &value)
+
+		switch parts[0] {
+		case "forward":
+			sub.Forward(value)
+		case "down":
+			sub.Down(value)
+		case "up":
+			sub.Up(value)
+		}
+	}
+
+	fmt.Printf("horizontal position is: %d, Depth: %d\n", sub.HorizontalPosition, sub.Depth)
+	fmt.Printf("multiplication of position and depth is: %d\n", sub.HorizontalPosition*sub.Depth)
+}
